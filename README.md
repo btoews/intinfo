919265511d2fee98a9da5e2d5555b3c550f3cf4b
Ben Toews <btoews@Bens-MacBook-Pro.local>
2012-08-21 14:07:45 -0500

    fixing packaging

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ intinfo
 =======
 
 Portable (between *nixes) information about network interfaces
+
+## Rebuilding Cython Portion ##
+To simplify distribution, the setup.py file builds from the `ioctl_requests.c`
+rather than `ioctl_requests.pyx`. To re-build the cython module, you could run
+
+    cython ./ioctl_requests.pyx
+
+or you could write a new setup.py like this:
+
+	from distutils.core import setup
+	from distutils.extension import Extension
+	from Cython.Distutils import build_ext
+
+	ext_modules = [Extension("ioctl_requests", ["ioctl_requests.pyx"])]
+
+	setup(
+	  name = 'ioctl_requests',
+	  cmdclass = {'build_ext': build_ext},
+	  ext_modules = ext_modules
+	)
--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from distutils.core import setup
-from distutils.extension import Extension
-#from setuptools import setup
-#from setuptools.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup
+from setuptools.extension import Extension
 
-ext_modules = [Extension("ioctl_requests", ["ioctl_requests.pyx"])]
+ext_modules = [Extension("ioctl_requests", ["ioctl_requests.c"])]
 
 setup(
   name = 'intinfo',
-  cmdclass = {'build_ext': build_ext},
+  description = 'Its hard to get network interface information in python in a portable manner. This should help, at least in *nix environments.',
+  version = '0.1',
+  license = 'GPL v3',
+  author = 'Ben Toews (mastahyeti)',
+  author_email = 'mastahyeti@gmail.com',
+  url = 'http://github.com/mastahyeti/intinfo',
   ext_modules = ext_modules,
   py_modules = ['intinfo'],
-
 )
